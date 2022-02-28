a531e6b8f9aff4916ba411bb8e59971cebb45a89
Conner Ward <ckward@uci.edu>
2022-02-28 13:05:04 -0800

    prettied nav

--- memepedia/src/components/Navbar.js
+++ memepedia/src/components/Navbar.js
@@ -1,17 +1,15 @@
-import {Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { BottomNavigation } from '@mui/material';
 import { BottomNavigationAction } from '@mui/material';
 
 function Navbar() {
     return (
         // this was my homemade one
-        <nav>
-            <div>
-                <Link to="/">SearchPage</Link>
-            </div>
-            <div>
-                <Link to="/meme">MemePage</Link>
-            </div>
+        // TODO: add image, style text under image
+        <nav className="nav">
+            <NavLink className="nav-item" activeClassName="nav-item.active" to="/">SearchPage</NavLink>
+            <NavLink className="nav-item" activeClassName="nav-item.active" to="/meme">MemePage</NavLink>
+            <div className="nav-item">Fake Option</div>
         </nav>
     );
 }
