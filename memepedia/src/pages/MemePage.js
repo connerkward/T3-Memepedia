9c360c0352051440d34f198d67fe5f8a831039aa
Hsun Ou Yang(Owen) <owenou506@gmail.com>
2022-03-01 14:11:09 -0800

    sample tree and tags added

--- memepedia/src/pages/MemePage.js
+++ memepedia/src/pages/MemePage.js
@@ -1,5 +1,6 @@
 import MemeCard from '../components/MemeCard'
-
+import MemeTree from '../components/MemeTree'
+import Tags from '../components/Tags'
 // import the image to use here..
 import img1 from '../resources/1_JzjHe3oYacjlJITIht-DQw.png'
 import img2 from '../resources/Best-Spider-Man-Memes.png.webp'
@@ -10,7 +11,10 @@ const MemePage = () => {
         <div>
             <h1>MemePage</h1>
             <MemeCard image={img2} creator={"Amy"}/>
+            <MemeTree/>
+            <Tags/>
         </div>
+            
     ) 
 };
 
